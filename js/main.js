1248418d46f70931f9611cf68727b7f56bbbb332
rieasebee <bee@riease.com>
2020-07-15 13:18:15 +0800

    0715twrr-1

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// var person = '小明'
+// var person2 = '杰倫'
+// person2 = person
+// console.log(person,person2)
+
+
+
+
+// var person = {
+//     name:'小明'
+// }
+
+// var person2 = person
+
+// person2.name = '杰倫'
+
+// console.log(person,person2)
+
+
+
+
+var family = {
+    name:"小明家",
+    members:{
+        father:"爸爸",
+        mon:"媽媽",
+        ming:"小明"
+    }
+}
+
+var member = family.members
+member ={ming:"大明"}
+console.log(member,family.members)
